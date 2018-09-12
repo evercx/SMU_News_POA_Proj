989bb8fddb6564fbadaf34d17aed15ecd0bbc528
salted-fish <13818195702@163.com>
2018-09-12 16:26:49 +0800

    merge

--- web/ipConfig.js
+++ web/ipConfig.js
@@ -2,16 +2,10 @@
  * Created by evercx on 16/9/20.
  */
 
-var mongo = {
-    user: "newsUser",
-    pwd: "1310724518",
-    host: "127.0.0.1",
-    port: "27017"
-};
 
 var IPAddress = {
-    mongodb:'mongodb://' + mongo.user + ':' 
-    + mongo.pwd + '@' + mongo.host + ':' + mongo.port + '/NewsPOA2/'
+    // mongodb:'mongodb://iotlab:iotlabmongodb@127.0.0.1:27034/NewsPOA/'
+    mongodb:'mongodb://iotlab:iotlabmongodb@127.0.0.1:27034/NewsPOA'
 };
 
 //exports.db = PSOCFI_DB;
