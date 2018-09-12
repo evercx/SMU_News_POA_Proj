891e7d92448ff9703713280b65369815cb441be0
salted-fish <13818195702@163.com>
2018-09-12 16:22:05 +0800

    web端添加mongo验证，请修改ipConfig.js

--- web/ipConfig.js
+++ web/ipConfig.js
@@ -2,8 +2,16 @@
  * Created by evercx on 16/9/20.
  */
 
+var mongo = {
+    user: "newsUser",
+    pwd: "1310724518",
+    host: "127.0.0.1",
+    port: "27017"
+};
+
 var IPAddress = {
-    mongodb:'mongodb://127.0.0.1:27017/NewsPOA2/'
+    mongodb:'mongodb://' + mongo.user + ':' 
+    + mongo.pwd + '@' + mongo.host + ':' + mongo.port + '/NewsPOA2/'
 };
 
 //exports.db = PSOCFI_DB;
